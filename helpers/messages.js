2b6098c92e1ab224043772a88ff9fd41c4779311
Matheus <90228238+ofmxtheuuz@users.noreply.github.com>
2023-01-16 11:16:05 -0300

    Update messages.js

--- helpers/messages.js
+++ helpers/messages.js
@@ -4,11 +4,5 @@ const chalk = require("chalk");
 module.exports = {
      serverMessage: function(message) {
           console.log(chalk.green("[mx-admin] ") + message)
-     },
-     errorMessage: function(error) {
-          console.log(chalk.red("Error: ") + error)
-     },
-     databaseMessage: function(message) {
-          console.log(chalk.green("Database: ") + message)
      }
 }
